84af4ecc38326007de698db0de7ee87e96f189be
Felipe <fliflaiflou10@gmail.com>
2024-10-16 14:06:37 -0300

    int map working

---
+++ src/map.c
@@ -0,0 +1,62 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   map.c                                              :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: fmontes <fmontes@student.42.fr>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/09/02 12:38:17 by felperei          #+#    #+#             */
+/*   Updated: 2024/10/15 13:35:29 by fmontes          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "../cub3d.h"
+
+#include <fcntl.h>  // Para open
+#include <unistd.h> // Para read, close
+#include <stdlib.h> // Para malloc, free
+
+
+
+// Função para ler o arquivo e retornar o conteúdo como um array de strings
+char **read_map(char *path) {
+    int fd = open(path, O_RDONLY);
+    if (fd == -1) return NULL;
+
+    char buffer[1024];
+    ssize_t bytes_read;
+    char *holder_map = malloc(1);
+    if (!holder_map) return NULL;
+
+    holder_map[0] = '\0';
+
+    // Ler o arquivo em partes
+    while ((bytes_read = read(fd, buffer, 1023)) > 0) {
+        buffer[bytes_read] = '\0';
+        char *temp = holder_map;
+        holder_map = ft_strjoin(holder_map, buffer);
+        free(temp);
+        if (!holder_map) return NULL;
+    }
+
+    close(fd);
+    char **map = ft_split(holder_map, '\n');
+    free(holder_map);
+
+    return map;
+}
+
+
+// int	size_map(t_data *dt)
+// {
+// 	int	i;
+
+// 	dt->w_map = ft_strlen(dt->map2d[0]);
+// 	i = 0;
+// 	while (dt->map2d[i])
+// 		i++;
+// 	dt->h_map = i;
+// 	if (dt->w_map != dt->h_map)
+// 		return (1);
+// 	return (0);
+// }
---
+++ src/move_player.c
@@ -0,0 +1,91 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   move_player.c                                      :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: fmontes <fmontes@student.42.fr>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/08/28 11:13:21 by felperei          #+#    #+#             */
+/*   Updated: 2024/10/15 15:06:29 by fmontes          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "../cub3d.h"
+
+void move_player_forward(t_mlx *mlx) {
+
+	int newX = (int)((mlx->ply->plyr_y ) / TILE_SIZE);
+	int newY = (int)((mlx->ply->plyr_x ) / TILE_SIZE);
+
+	if (newX >= 0 && newX < mlx->dt->h_map && newY >= 0 && newY < mlx->dt->w_map) {
+		if (mlx->dt->map[newX][newY] != 1) {
+			mlx->ply->plyr_x += 1 * PLAYER_SPEED;
+		}
+	}
+}
+
+void move_player_backward(t_mlx *mlx) {
+    double move_x = -cos(mlx->ply->angle) * PLAYER_SPEED;
+    double move_y = -sin(mlx->ply->angle) * PLAYER_SPEED;
+
+    int newX = (int)((mlx->ply->plyr_y + move_y) / TILE_SIZE);
+    int newY = (int)((mlx->ply->plyr_x + move_x) / TILE_SIZE);
+
+    if (newX >= 0 && newX <  mlx->dt->h_map  && newY >= 0 && newY < mlx->dt->w_map ) {
+        if (mlx->dt->map[newX][newY] != 1) {
+            mlx->ply->plyr_x += move_x;
+            mlx->ply->plyr_y += move_y;
+        }
+    }
+}
+
+
+void	rotate_player(t_mlx *mlx, int i)	// rotate the player
+{
+	if (i == 1)
+	{
+		mlx->ply->angle += ROTATION_SPEED; // rotate right
+		if (mlx->ply->angle > 2 * M_PI)
+			mlx->ply->angle -= 2 * M_PI;
+	}
+	else
+	{
+		mlx->ply->angle -= ROTATION_SPEED; // rotate left
+		if (mlx->ply->angle < 0)
+			mlx->ply->angle += 2 * M_PI;
+	}
+}
+
+
+int	keypress(int keycode, t_mlx *game)
+{
+	if (keycode == ESC || keycode == KEY_Q)
+		ft_exit(game);
+	game_events(keycode, game);
+	return (0);
+}
+
+void	game_events(int keycode, t_mlx *game)
+{
+	if (keycode == KEY_W || keycode == KEY_UP)
+	{
+		move_player_forward(game);
+		printf("W\n");
+		update_map(game);
+	}
+	else if (keycode == KEY_S || keycode == KEY_DOWN)
+	{
+		move_player_backward(game);
+		printf("S\n");
+	}
+	else if (keycode == KEY_D || keycode == KEY_RIGHT)
+	{
+		// game->ply->rot = 1;
+		rotate_player(game, 1);
+	}
+	else if (keycode == KEY_A || keycode == KEY_LEFT)
+	{
+		// game->ply->rot = -1;
+		rotate_player(game, 0);
+	}
+}
